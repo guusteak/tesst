aa8b68f17bf68fadad8e197b1582f4bc1f962d90
Paweł Gustaw <neruksiaz@gmail.com>
2022-09-02 21:34:55 +0200

    add JS card functionality

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ for(let i = 1; i<=5; i++){
         argumentpassed = i;
         validate(argumentpassed);
     }
-    document.getElementById("a"+i).addEventListener("click", checker);
+    document.getElementById("a"+i).addEventListener("input", checker);
 
 
 button.addEventListener("click", log);
@@ -39,10 +39,29 @@ function log () {
 }
 
 function validate (argumentpassed) {
+    switch(argumentpassed){
+            case 1:
+                document.querySelector("#name").innerHTML = document.getElementById("a1").value;
+                break;
+            case 2:
+                document.querySelector("#zeros").innerHTML = document.getElementById("a2").value;
+                break;
+            case 3:
+                document.querySelector("#month").innerHTML = document.getElementById("a3").value;
+                break;
+            case 4:
+                document.querySelector("#year").innerHTML = document.getElementById("a4").value;
+                break;
+            case 5:
+                document.querySelector("#CVC").innerHTML = document.getElementById("a5").value;
+                break;
+        }
+    
     if(document.getElementById("a"+argumentpassed).value!=""){
         document.getElementById("b"+argumentpassed).classList.add("none");
-        console.log("everything is ok!")
-    }
+        console.log("everything is ok!")}
+        
+    
     else{
 
         document.getElementById("b"+argumentpassed).classList.remove("none");   
